791586a53ebf29e08e59e84cdcfa842ead8bf3ab
Yiyi Wang <shd101wyy@sina.com>
2016-12-31 09:55:29 -0600

    epub-gen 0.0.17 is much slower than 0.0.16

--- example.js
+++ example.js
@@ -1,23 +1,23 @@
 const ebookCrawler = require('./index.js')
 
-let url = 'http://www.piaotian.net/html/0/738/'
+let url = 'http://www.piaotian.net/html/0/757/'
 ebookCrawler({
   url,
-  bookName: '宰执天下',
+  bookName: '赘婿',
   charset: 'gb2312', // resolve Chinese character issue
-  author: 'cuslaa',
-  cover: 'http://g.hiphotos.baidu.com/baike/w%3D268/sign=096c18fa1f30e924cfa49b3774096e66/472309f7905298227c6a3c31d7ca7bcb0b46d4e2.jpg',
+  author: '愤怒的香蕉',
+  cover: 'http://fdfs.xmcdn.com/group12/M03/DC/BE/wKgDW1aJ70HhURkfAAXL7-zgvgQ186.jpg',
   outputDir: './test',
   addFrontMatter: true,
   generateMarkdown: false,
 
   // $ is cheerio of the html
   table: function($) {
-    let toc = []
-    let as = $('.mainbody a')
+    const toc = []
+    const as = $('.mainbody a')
     for (let i = 5; i < as.length; i++) {
-      let a = $(as[i]),
-        bookUrl = url + a.attr('href'),
+      const a = $(as[i]),
+        bookUrl = url.replace(/\.index.html$/, '') + a.attr('href'),
         title = a.text().trim()
       toc.push({
         title: title,
@@ -33,7 +33,7 @@ ebookCrawler({
   // title is title...
   content: function($, title) {
     $('title, script, div, table, span, h1').remove()
-    let text = $('html').text().trim().replace('()',  '').replace(/\n    /g, '\n').replace(/\n/g, '<br>') // get content
+    const text = $('html').text().trim().replace('()',  '').replace(/\n    /g, '\n').replace(/\n/g, '<br />') // get content
     return text
   },
 })
